22a7a2af97285ac7ec68871a29841d0979a23f81
romanlatipov <romanlatipov@gmail.com>
2024-04-15 14:03:25 -0400

    make delete from playlist button

--- src/components/RenderPlaylists.jsx
+++ src/components/RenderPlaylists.jsx
@@ -9,9 +9,24 @@ export default function RenderPlaylists({playlistId, songSetter, setSrcChange}) 
         .then(data => setPlaylistSongs(data.songs))
     }, [playlistId])
 
+    function hanldeDeleteFromPlaylist(id, song) {
+        const temp = playlistSongs.filter(s => s.id != song.id);
+        setPlaylistSongs(temp);
+        fetch("http://localhost:3000/playlists/"+id, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                songs: [...temp]
+            })
+        })
+    }
 
-    console.log(playlistSongs);
-    const songs = playlistSongs.map(song => <Song song={song} songSetter={songSetter} setSrcChange={setSrcChange} button={<button style={{width: "160px"}}>Add to Playlist</button>}/>)
+    const songs = playlistSongs.map(song => <Song song={song} songSetter={songSetter} setSrcChange={setSrcChange} button={<button style={{width: "160px"}} onClick={() => {
+        hanldeDeleteFromPlaylist(playlistId, song);
+    }}>Delete</button>}/>)
     return(<>
         <h1>Playlist id is {playlistId}</h1>
         {songs}
